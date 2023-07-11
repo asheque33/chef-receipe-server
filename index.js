284b45db80c08634f4701a8d52f11e6fa12ce2af
asheque33 <ashequrrahman3333@gmail.com>
2023-07-12 02:32:36 +0600

    json data included

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
 
+// middleware 
+app.use(cors());
+
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
 res.send('Chef recipe server running')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs.chefs);
+});
+
+app.get('/chefs/:id', (req, res) => {
+const id = parseInt(req.params.id);
+// console.log(id); ei id(default~string) just route e(localhost:5000/chefs/13) hit korar id jeta json datay thakteo pare nao thakte pare;
+// res.send(id);
+if(id === 0){
+    res.send(chefs.chefs)
+}
+else{
+    const selectedChef = chefs.chefs.find(chef => parseInt(chef.id) === id) || {chef : null} ;
+    res.send(selectedChef);
+}
+
+
 });
 
 
